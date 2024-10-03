95a1e2dcbd99414cc4f291f961513b6b3dc92d70
Vu-Linh Nguyen <nguyenvulinh99@gmail.com>
2024-10-04 06:42:50 +0700

    chore: update requirements

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import multiprocessing
 import subprocess
 
 def run_fastapi():
-    subprocess.run(["uvicorn", "src.api.app_backend:app", "---host", "0.0.0.0", "--port", "8000"])
+    subprocess.run(["uvicorn", "src.api.app_backend:app", "--host", "0.0.0.0", "--port", "8000"])
 
 def run_streamlit():
     subprocess.run(["streamlit", "run", "./src/gui/app_gui.py"])
 
 if __name__ == "__main__":
-    fastapi_process = multiprocessing.Process(target = run_fastapi)
-    streamlit_process = multiprocessing.Process(target = run_streamlit)
+    fastapi_process = multiprocessing.Process(target=run_fastapi)
+    streamlit_process = multiprocessing.Process(target=run_streamlit)
 
     fastapi_process.start()
     streamlit_process.start()
